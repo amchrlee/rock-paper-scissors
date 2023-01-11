5d2114db67d4115678266eaba582fdc8519a45fa
Amy Lee <39871117+amchrlee@users.noreply.github.com>
2023-01-11 17:33:33 -0500

    Create playRound that plays single round of game
    This includes every possible outcome of wins, losses, and ties between
    playerSelection and computerSelection.

--- script.js
+++ script.js
@@ -8,4 +8,26 @@ const choices = [
     "scissors"
 ];
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {      
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        alert("It's a tie! You both chose rock.");
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        alert("You lose! Paper beats rock!");
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        alert("You win! Rock beats scissors!")
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        alert("It's a tie! You both chose paper.");
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        alert("You lose! Scissors beats paper!");
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        alert("You win! Paper beats rock!");
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        alert("It's a tie! You both chose scissors.");
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        alert("You lose! Rock beats scissors!");
+    } else {
+        alert("You win! Scissors beats paper!")
+    }
+}
