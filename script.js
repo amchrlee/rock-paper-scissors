6db631308447cd843c87324c6b9e43523ceb53a4
Amy Lee <39871117+amchrlee@users.noreply.github.com>
2023-01-31 16:02:58 -0500

    Create addElement to display results via DOM
    This function adds a div to display game results based on both
    playerSelection and computerSelection. Therefore, all console.logs and
    alerts were removed from the playRound function.

--- script.js
+++ script.js
@@ -8,33 +8,57 @@ const choices = [
     "scissors"
 ];
 
+const body = document.getElementsByTagName('body');
+
+function addElement(outcomeContent) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('outcome');
+    newDiv.appendChild(outcomeContent);
+    document.body.insertBefore(newDiv, body.lastElementChild);
+}
+
 function game(event) {
     let playerSelection = event.target.value;
     let computerSelection = getComputerChoice();
-    console.log("Computer: " + computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
+    playRound(playerSelection, computerSelection);
 };
 
-function playRound(playerSelection, computerSelection) {      
+function playRound(playerSelection, computerSelection) {
+
+    // Rock-Outcomes
     if (playerSelection == "rock" && computerSelection == "rock") {
-        alert("It's a tie! You both chose rock.");
+        let outcomeContent = document.createTextNode("It's a tie! You both chose rock.");
+        addElement(outcomeContent);
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        alert("You lose! Paper beats rock!");
+        let outcomeContent = document.createTextNode("You lose! Paper beats rock!");
+        addElement(outcomeContent);
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        alert("You win! Rock beats scissors!")
+        let outcomeContent = document.createTextNode("You win! Rock beats scissors!");
+        addElement(outcomeContent);
+
+    // Paper-Outcomes
     } else if (playerSelection == "paper" && computerSelection == "paper") {
-        alert("It's a tie! You both chose paper.");
+        let outcomeContent = document.createTextNode("It's a tie! You both chose paper.");
+        addElement(outcomeContent);
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        alert("You lose! Scissors beats paper!");
+        let outcomeContent = document.createTextNode("You lose! Scissors beats paper!");
+        addElement(outcomeContent);
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        alert("You win! Paper beats rock!");
+        let outcomeContent = document.createTextNode("You win! Paper beats rock!");
+        addElement(outcomeContent);
+
+    // Scissors-Outcomes
     } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        alert("It's a tie! You both chose scissors.");
+        let outcomeContent = document.createTextNode("It's a tie! You both chose scissors.");
+        addElement(outcomeContent);
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        alert("You lose! Rock beats scissors!");
+        let outcomeContent = document.createTextNode("You lose! Rock beats scissors!");
+        addElement(outcomeContent);
     } else {
-        alert("You win! Scissors beats paper!")
+        let outcomeContent = document.createTextNode("You win! Scissors beats paper!");
+        addElement(outcomeContent);
     }
+
 };
 
 const btns = document.querySelectorAll('button');
