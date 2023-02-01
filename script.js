a95865bfd66cb4eca4263e6a230c589c68c7f3f4
Amy Lee <39871117+amchrlee@users.noreply.github.com>
2023-02-01 17:32:57 -0500

    Modify and create functions for results display
    The main functions that impact the success of the results display are
    clearResultsMessage() and addResultsMessage(). These functions allow
    the outdated message from the previous round to be removed, and adds
    the current round results message to display.
    
    Other updates include: (1) change in logic for playRound() to
    structure the if-else statement to divide the conditions and actions by
    the results outcomes; (2) creation of capitalizeFirstLetter(); and
    (3) reorder of the layout of variables and functions for improved
    readability.

--- script.js
+++ script.js
@@ -1,64 +1,50 @@
-function getComputerChoice() {
-    return choices[Math.floor(Math.random()*choices.length)];
-};
-
 const choices = [
     "rock", 
     "paper", 
     "scissors"
 ];
+const resultsMessageDiv = document.querySelector('.message');
 
-const body = document.getElementsByTagName('body');
+function getComputerChoice() {
+    return choices[Math.floor(Math.random()*choices.length)];
+};    
 
-function addElement(outcomeContent) {
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('outcome');
-    newDiv.appendChild(outcomeContent);
-    document.body.insertBefore(newDiv, body.lastElementChild);
+function capitalizeFirstLetter(str) {
+    const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
+    return capitalized;
 }
 
 function game(event) {
     let playerSelection = event.target.value;
     let computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
-};
+};    
+
+function clearResultsMessage() {
+    resultsMessageDiv.innerHTML = '';
+}
+
+function addResultsMessage(resultsMessage) {
+    resultsMessageDiv.appendChild(resultsMessage);
+}
 
 function playRound(playerSelection, computerSelection) {
-
-    // Rock-Outcomes
-    if (playerSelection == "rock" && computerSelection == "rock") {
-        let outcomeContent = document.createTextNode("It's a tie! You both chose rock.");
-        addElement(outcomeContent);
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        let outcomeContent = document.createTextNode("You lose! Paper beats rock!");
-        addElement(outcomeContent);
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        let outcomeContent = document.createTextNode("You win! Rock beats scissors!");
-        addElement(outcomeContent);
-
-    // Paper-Outcomes
-    } else if (playerSelection == "paper" && computerSelection == "paper") {
-        let outcomeContent = document.createTextNode("It's a tie! You both chose paper.");
-        addElement(outcomeContent);
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        let outcomeContent = document.createTextNode("You lose! Scissors beats paper!");
-        addElement(outcomeContent);
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        let outcomeContent = document.createTextNode("You win! Paper beats rock!");
-        addElement(outcomeContent);
-
-    // Scissors-Outcomes
-    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        let outcomeContent = document.createTextNode("It's a tie! You both chose scissors.");
-        addElement(outcomeContent);
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        let outcomeContent = document.createTextNode("You lose! Rock beats scissors!");
-        addElement(outcomeContent);
-    } else {
-        let outcomeContent = document.createTextNode("You win! Scissors beats paper!");
-        addElement(outcomeContent);
+    if (playerSelection === computerSelection) {    // tie
+        let resultsMessage = document.createTextNode("It's a tie! You both chose " + playerSelection + ".");
+        clearResultsMessage();
+        addResultsMessage(resultsMessage);
+    } else if (     // loser
+        (playerSelection == "rock" && computerSelection == "paper") || 
+        (playerSelection == "paper" && computerSelection == "scissors") || 
+        (playerSelection == "scissors" && computerSelection == "rock")) {
+        let resultsMessage = document.createTextNode("You lose! " + capitalizeFirstLetter(computerSelection) + " beats " + playerSelection + ".");
+        clearResultsMessage();
+        addResultsMessage(resultsMessage);
+    } else {    // winner
+        let resultsMessage = document.createTextNode("You win! " + capitalizeFirstLetter(playerSelection) + " beats " + computerSelection + ".");
+        clearResultsMessage();
+        addResultsMessage(resultsMessage);
     }
-
 };
 
 const btns = document.querySelectorAll('button');
