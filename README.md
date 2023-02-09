dabaa1ffa6d59cb6d1e9a5c4f8513b722e302ee7
Amy Lee <39871117+amchrlee@users.noreply.github.com>
2023-02-09 14:28:47 -0500

    Add CSS and edit JS to apply fairy tale theme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# rock-paper-scissors
+# Rock-Paper-Scissors Game
+
+This project is a web-based implementation of the classic Rock Paper Scissors game to be played against the computer. To add a creative spin on the game, I redesigned it into a fairy tale theme as Knight Princess Dragon, with similar gameplay. The aim of this project was to begin practicing and applying my JavaScript knowledge.
+
+The project was referenced from The Odin Project Foundations curriculum: [Part 1](https://www.theodinproject.com/lessons/foundations-rock-paper-scissors) and [Part 2](https://www.theodinproject.com/lessons/foundations-revisiting-rock-paper-scissors)
+
+## Live Demo
+
+Preview :link:
+
+## How to Play
+
+The player is prompted to make their move by clicking one of three buttons. Meanwhile, the computer will randomly make its own selection for its move. This makes up one round of the game. The results of the round will be displayed in the box above the buttons. Whoever wins the round gets a point that is added to the score counter. The first player to earn five points wins the game. If the player wants to play again, they must refresh the page.
+
+## Languages/Technologies
+
+* HTML/CSS/JavaScript
+* Visual Studio Code
+* Linux terminal
+* Git and GitHub
+* Chrome Developer tools
+* Google Fonts
+
+## Development Process
+
+1. I started by making a simple implementation of Rock Paper Scissors that allows the user to play the game from the browser console. This involved coding the basic logic of the game.
+2. After successfully getting the game to work, I planned and designed the user interface by sketching the layout in a OneNote file, including text, buttons, displays, color gradients, images, and other relevant elements. This is when I came up with the idea to reform the game into Knight Princess Dragon.
+3. After the planning stage, I rewrote some portions of the code to remove all references to console.log, and built the GUI based off of the design. This included some rework of the functions to include DOM methods that would display content on the page.
+
+## Project Status
+
+In process -- Although the functionality of the game is working correctly, some design additions are in the process of being completed.
+
+## What I learned
+* Using the DOM to manipulate elements and styles
+* Implementing event listeners to listen and respond to user actions
+* Applying knowledge of JavaScript fundamentals to code a working game
+* Refactoring code and breaking down larger portiosn of the program into smaller functions
+* Using programming best practices to improve code structure and readability
--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 const choices = [
-    "rock", 
-    "paper", 
-    "scissors"
+    "knight", 
+    "princess", 
+    "dragon"
 ];
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
-};
-
-function capitalizeFirstLetter(str) {
-    const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
-    return capitalized;
 };
 
 function playRound(event) {
@@ -36,27 +31,53 @@ function updateScore() {
     computerScoreDiv.textContent = `${computerScore}`;
 };
 
+let knightDefeatsDragon = 
+    "The knight defeats the dragon. During the grueling fight over the princess, the dragon's wings were clipped by the knight's axe, leading to its dramatic downfall...";
+let princessDefeatsKnight = 
+    "The princess defeats the knight. He was overcome by her beauty, and instantly falls... in love.";
+let dragonDefeatsPrincess = 
+    "The dragon defeats the princess. She attempted to climb out of the tower's window, only to be caught in the act by the dragon, releasing his fiery breath. He was quite heated, to say the least...";
+
 function getWinnerOfRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {    // tie
-        let resultsMessage = document.createTextNode("It's a tie! You both chose " + playerSelection + ".");
+    if               // tie
+    (playerSelection === computerSelection) {
+        let resultsMessage = document.createTextNode("It's a tie! You both chose the " + playerSelection + ". Both get confused why they see another version of themselves in the flesh.");
         resetResultsMessage(resultsMessage);
 
-    } else if (     // loser
-        (playerSelection == "rock" && computerSelection == "paper") || 
-        (playerSelection == "paper" && computerSelection == "scissors") || 
-        (playerSelection == "scissors" && computerSelection == "rock")) {
-        let resultsMessage = document.createTextNode("You lose! " + capitalizeFirstLetter(computerSelection) + " beats " + playerSelection + ".");
+    } else if       // loser
+    (playerSelection == "knight" && computerSelection == "princess") {
+        let resultsMessage = document.createTextNode("You lose! " + princessDefeatsKnight);
         resetResultsMessage(resultsMessage);
         computerScore++;
-        
-    } else {    // winner
-        let resultsMessage = document.createTextNode("You win! " + capitalizeFirstLetter(playerSelection) + " beats " + computerSelection + ".");
+    } else if       // loser
+    (playerSelection == "princess" && computerSelection == "dragon") {
+        let resultsMessage = document.createTextNode("You lose! " + dragonDefeatsPrincess);
+        resetResultsMessage(resultsMessage);
+        computerScore++;
+    } else if       // loser
+    (playerSelection == "dragon" && computerSelection == "knight") {
+        let resultsMessage = document.createTextNode("You lose! " + knightDefeatsDragon);
+        resetResultsMessage(resultsMessage);
+        computerScore++;
+    
+    } else if       // winner
+    (playerSelection == "knight" && computerSelection == "dragon") {
+        let resultsMessage = document.createTextNode("You win! " + knightDefeatsDragon);
+        resetResultsMessage(resultsMessage);
+        playerScore++;
+    } else if       // winner
+    (playerSelection == "princess" && computerSelection == "knight") {
+        let resultsMessage = document.createTextNode("You win! " + princessDefeatsKnight);
+        resetResultsMessage(resultsMessage);
+        playerScore++;
+    } else {        // winner
+        let resultsMessage = document.createTextNode("You win! " + dragonDefeatsPrincess);
         resetResultsMessage(resultsMessage);
         playerScore++;
     }
 };
 
-const endGameMessageDiv = document.querySelector('.end-game-message');
+const endGameMessageDiv = document.querySelector('.end-round-message');
 const btns = document.querySelectorAll('button');
 const disableButtons = () => {
     btns.disabled = true;
@@ -67,7 +88,7 @@ function getWinnerOfGame () {
         endGameMessageDiv.textContent = `CONGRATULATIONS! You win the game! Please refresh the page to play again.`;
         btns.addEventListener('click', disableButtons);
     } else if (computerScore === 5) {
-        endGameMessageDiv.textContent = `GAME OVER. You lose the game. Please refresh the page to play again.`;
+        endGameMessageDiv.textContent = `GAME OVER. You lose. Please refresh the page to play again.`;
         btns.addEventListener('click', disableButtons);
     }
 };
